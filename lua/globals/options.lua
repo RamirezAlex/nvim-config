472be3069f92167929443f25a0365659c6d13093
Alex Ramirez <alexramirez@Alexs-MacBook-Pro-2.local>
2022-08-06 14:39:23 -0500

    First commit

---
+++ lua/globals/options.lua
@@ -0,0 +1,33 @@
+vim.o.exrc = true  -- exec a local vimrc (like direnv)
+vim.o.relativenumber = true  -- relative number in gutter
+vim.o.nu = true  -- Indent current line number
+vim.o.hlsearch = false  -- Don't highlight search
+vim.o.incsearch = true  -- jump incrementally to search results
+vim.o.hidden = true -- allow hidden buffers
+vim.o.errorbells = false -- ding ding!
+vim.o.wrap = false -- don't wrap long lines
+vim.o.swapfile = false -- don't need no swap files!
+vim.o.backup = false  -- ... or a backup!
+vim.o.undodir = "~/.nvim/undodir"  -- sets where undo files land
+vim.o.scrolloff = 8  -- scroll offwidth so it's not the very bottom
+vim.o.signcolumn = "yes" -- always set the far left bar/column
+-- vim.o.nocompatible = true
+vim.o.mouse = "a"  -- always enable mouse mode
+vim.o.tabstop = 2
+vim.o.softtabstop = 2
+vim.o.shiftwidth = 2
+vim.o.expandtab = true
+vim.o.smartindent = true
+
+vim.o.ignorecase = true  -- ignore case when searching
+vim.o.smartcase = true  -- Unless we explicitly use cases in search
+vim.o.splitbelow = true  -- automagically h split going down
+vim.o.splitright = true -- automagically v split going right
+vim.opt.listchars = {  -- see hidden chars and their colors
+  tab = "| ",
+  eol = '¬',
+  trail = '·'
+}
+vim.o.list = true
+vim.o.updatetime = 100
+
---
+++ plugin/telescope.lua
@@ -0,0 +1,10 @@
+require('telescope').load_extension('fzf')
+
+local options = { noremap = true }
+
+vim.api.nvim_set_keymap('n', '<leader>ff', '<cmd>Telescope find_files<cr>', options)
+vim.api.nvim_set_keymap('i', 'jk', '<ESC>', options)
+vim.api.nvim_set_keymap('n', '<leader>fg', '<cmd>Telescope live_grep<cr>', options)
+vim.api.nvim_set_keymap('n', '<leader>fb', '<cmd>Telescope buffers<cr>', options)
+vim.api.nvim_set_keymap('n', '<leader>fh', '<cmd>Telescope help_tags<cr>', options)
+
