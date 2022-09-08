d210d9bbd0efb1a54321fdf6529e636acc9eb29e
Alex Ramirez <alexander.ramirez@gmail.com>
2022-09-08 02:04:41 -0500

    Add Telescop remaps

--- plugin/telescope.lua
+++ plugin/telescope.lua
@@ -2,9 +2,12 @@ require('telescope').load_extension('fzf')
 
 local options = { noremap = true }
 
-vim.api.nvim_set_keymap('n', '<leader>ff', '<cmd>Telescope find_files<cr>', options)
 vim.api.nvim_set_keymap('i', 'jk', '<ESC>', options)
+vim.api.nvim_set_keymap('i', 'jj', '<ESC>', options)
+vim.api.nvim_set_keymap('n', '<leader>ff', '<cmd>Telescope find_files<cr>', options)
 vim.api.nvim_set_keymap('n', '<leader>fg', '<cmd>Telescope live_grep<cr>', options)
 vim.api.nvim_set_keymap('n', '<leader>fb', '<cmd>Telescope buffers<cr>', options)
 vim.api.nvim_set_keymap('n', '<leader>fh', '<cmd>Telescope help_tags<cr>', options)
+vim.api.nvim_set_keymap('n', '<leader>fr', '<cmd>Telescope resume<cr>', options)
+vim.api.nvim_set_keymap('n', '<leader>fs', '<cmd>Telescope grep_string<cr>', options)
 
