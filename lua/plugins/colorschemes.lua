b269e7ee1f90880194e14aca7ca1a1d72537ad48
Alex Ramirez <alexander.ramirez@gmail.com>
2023-05-03 00:00:31 -0500

    Refactor colorschemes

---
+++ lua/plugins/colorschemes.lua
@@ -0,0 +1,20 @@
+return {
+  { "EdenEast/nightfox.nvim" },
+  { "catppuccin/nvim", name = "catppuccin" },
+  { "ellisonleao/gruvbox.nvim" },
+  { "rose-pine/neovim", name = "rose-pine" },
+  {
+    "LazyVim/LazyVim",
+    opts = {
+      -- colorscheme = "catppuccin-mocha",
+      -- colorscheme = "gruvbox",
+      -- colorscheme = "rose-pine",
+      -- colorscheme = "terafox",
+      -- colorscheme = "duskfox",
+      -- colorscheme = "carbonfox",
+      colorscheme = "nightfox",
+      -- colorscheme = "nordfox",
+      -- colorscheme = "tokyonight",
+    },
+  },
+}
