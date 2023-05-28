7be8df8ab7d8f18e5c4ee427d6351db966837866
Alex Ramirez <alexander.ramirez@gmail.com>
2023-05-27 21:10:49 -0500

    Add DAP

--- lua/plugins/rust-tools.lua
+++ lua/plugins/rust-tools.lua
@@ -1,3 +1,10 @@
+-- local mason_registry = require("mason-registry")
+--
+-- local codelldb = mason_registry.get_package("codelldb")
+-- local extension_path = codelldb:get_install_path() .. "/extension"
+-- local codelldb_path = extension_path .. "/adapter/codelldb"
+-- local liblldb_path = extension_path .. "/lldb/lib/liblldb.dylib"
+
 return {
   {
     "simrat39/rust-tools.nvim",
@@ -108,7 +115,9 @@ return {
         }, -- rust-analyer options
 
         -- debugging stuff
-        dap = {},
+        dap = {
+          -- adapter = require("rust-tools.dap").get_codelldb_adapter(codelldb_path, liblldb_path),
+        },
       },
       config = function(_, opts)
         require("rust-tools").setup(opts)
